5f3f7df1014ae7751dcb97879dce53c20923f7b9
Charlie <charlie.e.spencer@gmail.com>
2023-02-19 20:18:36 +0000

    Fix deadzone so no longer gets stuck in motion

--- classes/RadioControl.py
+++ classes/RadioControl.py
@@ -7,8 +7,8 @@ import json
 mapping = {
     # "LeftX":("ROLL_R",90,5,255),
     # "LeftY":("DRIVE",90,5,255),
-    "RightX":("CAM_PAN",90,5,10),
-    "RightY":("CAM_TILT",90,5,10),
+    "RightX":("CAM_PAN",90,5,15),
+    "RightY":("CAM_TILT",90,5,15),
     # "LeftDial":("TURN",0,5,255),
 }
 
@@ -44,12 +44,13 @@ class RadioControl:
                     deadzone=mapping[channel][2]
                     multiplier=mapping[channel][3]
                     value=rawvalue-centre
-                    if value < centre-deadzone/2 or value > centre+deadzone/2:
+                    if value < 0-deadzone/2 or value > 0+deadzone/2:
                         value=value/90
                         value=value*multiplier
                         constrain(value,-multiplier,multiplier)
-                        self.commands.update({command:value})
-                        self.output.write("RC",f"{command.ljust(6)}: {value}",False)
-            
+                    else:
+                        value=0
+                    self.commands.update({command:value})
+                    self.output.write("RC",f"{command.ljust(6)}: {value}",False)
 
                 
